bc9e8162ccd6796aa48780a9dd590b27ea40c6a9
Lakshay <lakshayyadav2006@gmail.com>
2022-02-07 10:10:05 +0530

    Got the using of items functionality to work.

--- Assets/Scripts/Item Usage Management/ItemUser.cs
+++ Assets/Scripts/Item Usage Management/ItemUser.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using System.Linq;
 
 public class ItemUser : MonoBehaviour
 {
@@ -20,22 +21,28 @@ public class ItemUser : MonoBehaviour
     
     private void Update()
     {
-        // if (Input.GetKeyDown(KeyCode.Alpha1)) currentIndex = 0;
-        // if (Input.GetKeyDown(KeyCode.Alpha2)) currentIndex = 1;
-        // if (Input.GetKeyDown(KeyCode.Alpha3)) currentIndex = 2;
-        //
-        // currentItem = itemsInInventory[currentIndex];
-        //
-        // if (Input.GetMouseButtonDown(1))
-        // {
-        //     if (currentItem != null)
-        //         currentItem.Use();
-        // }
+        if (Input.GetKeyDown(KeyCode.Alpha1)) currentIndex = 0;
+        if (Input.GetKeyDown(KeyCode.Alpha2)) currentIndex = 1;
+        if (Input.GetKeyDown(KeyCode.Alpha3)) currentIndex = 2;
+
+        if (itemsInInventory.Count > 0)
+        {
+            currentItem = itemsInInventory[currentIndex];
+        }
+        
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log(currentItem);
+            if (currentItem != null)
+                currentItem.Use();
+        }
     }
 
     public void PickupItem(GameObject pickup)
     {
         var item = Instantiate(pickup, itemHolder);
+        foreach (var i in item.GetComponents<MonoBehaviour>().OfType<IItem>())
+            AddItem(i);
     }
     
     public void AddItem(IItem item)
